4efa8a96a110f81cb843d24a916af252e4fc2af5
gmalulu <gracemalulu@gmail.com>
2023-04-02 23:47:56 +0100

    adding send notif request

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\UserData;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,10 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class NotificationController extends AbstractController
 {
-    private $notifyer;
+    
 
-    public function __construct(HttpClientInterface $notifyer){
-        $this->$notifyer = $notifyer;
+    public function __construct(private HttpClientInterface $client, private EntityManagerInterface $em){
+       
 
     }
     #[Route('/notification', name: 'app_notification')]
@@ -27,17 +28,29 @@ class NotificationController extends AbstractController
     #[Route('/notification/all', name: 'app_notification_all')]
     public function notifyAll(): Response
     {
-        $manager = $this->getDoctrine()->getManager();
-        $users = $manager->getRepository(UserData::class)->findAll();
-        $this->notifyer->request('POST', 'https://...', [
+        
+        $users = $this->em->getRepository(UserData::class)->findAll();
+        $response=$this->client->request('POST', 'https://fcm.googleapis.com/fcm/send', [
             'headers' => [
                 'Content-Type' => 'application/json',
-                'Authorization'=> 'bearer '
+                'Authorization'=> 'key=AAAA74CDm8k:APA91bHNL1Hoi9UXIRpfRTW-8Oc36azqS58RmRFtMUcswrYo_IRhErI9S1SRR5NJKPnepCuSOvdLOhZv6vBfyeflG9KPo0HACncQjaXQ3xdMKhjnRX6n-j_-YDZgu3iL3xLxUfGoDfrj'
             ],
-            'body'=>
-        ])
-        return $this->render('notification/index.html.twig', [
-            'controller_name' => 'NotificationController',
+            
+            'json'=>[
+ 
+                "registration_ids"=>["cFnSWQxnRb-KTtdG5jO8El:APA91bFwQU2w1kmWuPJC5H-wci7xrGuPVoRXx4-MIb6rr7PWm28id_kQG7pTe5mvVqIO_GZcvzzpfiheDwv3F-Z2ULpUfNnPm8RSLK25yxOGOlF8OJADiy-jlFOfCNcsFrS7kaosU3Av","ceWqCGwkS42yXsi7D3m5L9:APA91bG8YNS5_7XTA1Gc8AMoSbMwXCoZ_YeDiAkAYIgAUAtaNnAMxKiPrp-Tn-ByBsGS2AEedQ3Wv306lmzyVd4y0I53S5TlMHf4pHuxP4iC29cwnwQU-63uc9cU33XI0NOsoltIUM1X"],
+                "notification" => [
+                    "body" => "Upload recomandation",
+                    "title"=> "You need to upload pastor letter to be verified"
+                ],
+                "data" => [
+                    "body" => "You need to upload pastor letter to be verified",
+                    "title"=>"Upload recomandation",
+                    "type" => "PASTOR_URL"
+                ]
+            ]
         ]);
+        return $this->json(['success'=>$response->getContent()],$response->getStatusCode());
     }
+    
 }
