b04b53a577085bd5ca8adad4f6e5df81abd2c395
BN-Mara <sheltonsflash@gmail.com>
2023-11-17 16:42:31 +0100

    changing maxicash access

--- src/Helper/PaymentUrl.php
+++ src/Helper/PaymentUrl.php
@@ -9,7 +9,7 @@ class PaymentUrl{
     public function paymentUrl(Payment $payment, string $host): string{
         
         $postData = ["PayType"=>"MaxiCash","Amount"=>$payment->getAmount() * 100,"Currency"=>$payment->getCurrency(),
-        "Telephone"=>$payment->getPhone(),"Email"=>$payment->getEmail(),"MerchantID"=>"2bd7fd5caedc48dd8c5bcabee629812b","MerchantPassword"=>"55a6046137584680abddafe262985ff2",
+        "Telephone"=>$payment->getPhone(),"Email"=>$payment->getEmail(),"MerchantID"=>"4bc27182c02e4d159f1179d10b3537af","MerchantPassword"=>"deaea1d899d24f9ea47b175ce8d1a4ae",
         "Language"=>"fr","Reference"=>$payment->getReference(),"Accepturl"=>$host."/vote/process/success",
         "Cancelurl"=>$host."/vote/process/cancel","Declineurl"=>$host."/vote/process/fail",
         "NotifyURL"=>$host."/vote/process/notify"];
